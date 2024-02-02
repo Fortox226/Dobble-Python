5a36e022dee6b7d98e34a46d870e08bb918e433b
Fortox226 <97980099+Fortox226@users.noreply.github.com>
2024-02-02 10:20:46 +0100

    + first lobby window

--- main.py
+++ main.py
@@ -3,7 +3,22 @@ import pygame.gfxdraw
 
 
 def main():
-    
+    pygame.init()
+    screen = pygame.display.set_mode((500, 500))
+    screen.fill((255, 0, 0))
+
+    try:
+        while True:
+            event = pygame.event.wait()
+            if event.type == pygame.QUIT:
+                break
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE or event.unicode == "q":
+                    break
+            pygame.display.flip()
+    finally:
+        pygame.quit()
+
 
 if __name__ == "__main__":
     main()
