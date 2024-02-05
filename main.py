40099b1c7ac24f51b1d6b7b14abde06550791e79
Fortox226 <97980099+Fortox226@users.noreply.github.com>
2024-02-05 13:35:05 +0100

    asd

--- main.py
+++ main.py
@@ -6,7 +6,10 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((1280, 720))
     screen.fill(('white'))
+    pygame.font.init()
 
+    font = pygame.font.SysFont("Comic Sans MS", 24)
+    text = font.render("Tekst w \"pygame\"", False, [128, 64, 255])
     try:
         while True:
             event = pygame.event.wait()
@@ -17,6 +20,7 @@ def main():
                     break
             pygame.display.flip()
     finally:
+        screen.blit(text, [0, 0])
         pygame.quit()
 
 
